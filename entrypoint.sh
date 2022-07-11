99619906181f94233b00673e4914ea28fe67a23e
Conrad <grosserconrad@gmail.com>
2022-07-11 14:11:40 +0200

    Removed git setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,23 +3,6 @@
 # x would be for showing the commands before they are executed
 set -eux
 
-# FUNCTIONS
-# Function for setting up git env in the docker container (copied from https://github.com/stefanzweifel/git-auto-commit-action/blob/master/entrypoint.sh)
-_git_setup ( ) {
-    cat <<- EOF > $HOME/.netrc
-      machine github.com
-      login "$GITHUB_ACTOR"
-      password "$GITHUB_TOKEN"
-      machine api.github.com
-      login "$GITHUB_ACTOR"
-      password "$GITHUB_TOKEN"
-EOF
-    chmod 600 $HOME/.netrc
-
-    git config --global user.email "actions@github.com"
-    git config --global user.name "GitHub Action"
-}
-
 echo "Installing dependencies..."
 pip3 install -U pip wheel pygraphviz
 pip3 install -r "$INPUT_PIP_PATH"
@@ -30,8 +13,6 @@ python3 manage.py graph_models -a -g -o "$INPUT_OUTPUT_PATH"
 
 # To keep runtime good, just continue if something was changed
 if [[ `git status --porcelain` ]]; then
-    # Calling method to configure the git environemnt
-    _git_setup
     # Commit and push changes back
     git commit -am "$INPUT_COMMIT_MESSAGE" --author="$GITHUB_ACTOR <$GITHUB_ACTOR@users.noreply.github.com>"
     git push origin
